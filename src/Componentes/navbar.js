3e0fa3c024e70958537e053479ae42f61d3e83f9
Mart994 <38144617+Mart994@users.noreply.github.com>
2021-04-19 17:16:40 -0300

    Update navbar.js

--- src/Componentes/navbar.js
+++ src/Componentes/navbar.js
@@ -3,7 +3,6 @@ import { React } from "react";
 const Navbar = () => {
     let imgUrl = "https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png"
     const handleClick =() => {
-        console.log("apretaste el logo");
         window.location = '/';
     }
     
