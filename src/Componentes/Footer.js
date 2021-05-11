5329fbea00882ab0cc3db956a69ba6a61d807799
Mart994 <38144617+Mart994@users.noreply.github.com>
2021-05-11 14:20:19 -0300

    background-update

--- src/Componentes/Footer.js
+++ src/Componentes/Footer.js
@@ -3,7 +3,10 @@ import React from "react";
 const Footer = () => (
   <footer>
     <div>
-        <h3>Creado por <a href="https://github.com/Mart994/">MartinGimenez</a> </h3>
+        <h3>Creado por <a href="https://github.com/Mart994/pokereactjs/">MartinGimenez</a> </h3>
+    </div>
+    <div>
+      <h5>Credits: <a href="https://pokeapi.co/">PokeApi</a></h5>
     </div>
   </footer>
 );
