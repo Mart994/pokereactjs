eac9106e0e979340076a1b101ccc65d2de36b414
Mart994 <38144617+Mart994@users.noreply.github.com>
2021-05-13 12:17:54 -0300

    Update Footer.js

--- src/Componentes/Footer.js
+++ src/Componentes/Footer.js
@@ -3,7 +3,8 @@ import React from "react";
 const Footer = () => (
   <footer>
     <div>
-        <h3>Creado por <a href="https://github.com/Mart994/pokereactjs/">MartinGimenez</a> </h3>
+        <h3>Creado por <a href="https://mgimenez-portfolio.netlify.app/">MartinGimenez</a> </h3>
+        <h3>Repo en <a href="https://github.com/Mart994/pokereactjs">Github</a> </h3>
     </div>
     <div>
       <h5>Credits: <a href="https://pokeapi.co/">PokeApi</a></h5>
